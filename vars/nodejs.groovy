db2e1650ade3616320cffb3c2e47e1437c9befb8
surendra reddy <surendrareddylamuru01@gmail.com>
2023-01-31 12:54:36 +0530

    mbranch.yml

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,67 +1,62 @@
 def call() {
     pipeline {
-
-        agent any
-
-        stages {
-
+            sh 'env'
             common.codequality()
 
-            stage('Style Checks') {
-                when {
-                    anyOf{
-                        branch 'main'
-                        tag "*"
-                    }
-                }
-                steps {
-                    echo 'Code Quality'
-                    sh 'env'
-                }
-
-            }
-
-
-            stage('unit tests') {
-                when {
-                    anyOf{
-                        branch 'main'
-                        tag "*"
-                    }
-                }
-                steps {
-                    echo 'unit tests'
-                }
-
-            }
-
-
-            stage('Download Dependencies') {
-                when { tag "*" }
-                steps {
-                    echo 'download dependencies'
-                }
-
-            }
-
-
-            stage('Prepare Artifacts') {
-                when { tag "*" }
-                steps {
-                    echo 'prepare artifacts'
-                }
-
-            }
-
-
-            stage('Publish Artifacts') {
-                when { tag "*" }
-                steps {
-                    echo 'publish artifacts'
-                }
-
-            }
-
-        }
-    }
-}
+//            stage('Style Checks') {
+//                when {
+//                    anyOf{
+//                        branch 'main'
+//                        tag "*"
+//                    }
+//                }
+//                steps {
+//                    echo 'Code Quality'
+//                    sh 'env'
+//                }
+//
+//            }
+//
+//
+//            stage('unit tests') {
+//                when {
+//                    anyOf{
+//                        branch 'main'
+//                        tag "*"
+//                    }
+//                }
+//                steps {
+//                    echo 'unit tests'
+//                }
+//
+//            }
+//
+//
+//            stage('Download Dependencies') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'download dependencies'
+//                }
+//
+//            }
+//
+//
+//            stage('Prepare Artifacts') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'prepare artifacts'
+//                }
+//
+//            }
+//
+//
+//            stage('Publish Artifacts') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'publish artifacts'
+//                }
+//
+//            }
+//
+//    }
+//}
