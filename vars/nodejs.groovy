c9f2a8198a9b16e108265d5090926f40f10cad88
surendra reddy <surendrareddylamuru01@gmail.com>
2023-01-31 14:08:03 +0530

    mbranch.yml

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -7,7 +7,7 @@ def call() {
             sh 'env'
             common.codequality()
 
-            if ( BRANCH_NAME == "main" || TAG_NAME ==~ "*") {
+            if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*") {
                 stage('Style Checks') {
                     echo 'style checks'
                 }
