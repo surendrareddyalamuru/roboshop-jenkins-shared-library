9d4e94d07b5f30004fb4c0e7b875c6a59d18c9df
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-02 11:15:07 +0530

    jenkinsfile

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,7 +1,11 @@
 def call() {
+    env.SONAR_EXTRA_OPTS = "-Dsonar.java.binaries=./target"
     node {
         sh 'env'
         common.codeCheckout()
+        stage('Compile Code') {
+            sh 'mvn compile'
+        }
         common.codequality()
         common.codechecks()
         common.artifacts()
