2a09227ea869c0e9cc42330e5a0e5ca8ab6c7809
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-02 11:19:09 +0530

    jenkinsfile

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -1,64 +1,10 @@
 def call() {
-    pipeline {
+    node {
+        sh 'env'
+        common.codeCheckout()
+        common.codequality()
+        common.codechecks()
+        common.artifacts()
 
-        agent any
-
-        stages {
-
-            stage('Code quality') {
-                steps {
-                    echo 'Code Quality'
-                }
-
-            }
-
-
-
-            stage('Style Checks') {
-                when {
-                    anyOf{
-                        branch 'main'
-                        tag "*"
-                    }
-                }
-                steps {
-                    echo 'Code Quality'
-                    sh 'env'
-                }
-
-            }
-
-
-            stage('unit tests') {
-                when {
-                    anyOf{
-                        branch 'main'
-                        tag "*"
-                    }
-                }
-                steps {
-                    echo 'unit tests'
-                }
-
-            }
-
-            stage('Prepare Artifacts') {
-                when { tag "*" }
-                steps {
-                    echo 'prepare artifacts'
-                }
-
-            }
-
-
-            stage('Publish Artifacts') {
-                when { tag "*" }
-                steps {
-                    echo 'publish artifacts'
-                }
-
-            }
-
-        }
     }
 }
