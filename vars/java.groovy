f8cc5e6a1e22fdf2272b0ecad5339c09e4a4bd0a
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-02 11:07:44 +0530

    jenkinsfile

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,80 +1,10 @@
 def call() {
-    pipeline {
+    node {
+        sh 'env'
+        common.codeCheckout()
+        common.codequality()
+        common.codechecks()
+        common.artifacts()
 
-        agent any
-
-        stages {
-
-            stage('compile code') {
-                steps {
-                    echo 'compile code'
-                }
-
-            }
-            stage('Code quality') {
-                steps {
-                    echo 'Code Quality'
-                }
-
-            }
-
-
-
-            stage('Style Checks') {
-                when {
-                    anyOf{
-                        branch 'main'
-                        tag "*"
-                    }
-                }
-                steps {
-                    echo 'Code Quality'
-                    sh 'env'
-                }
-
-            }
-
-
-            stage('unit tests') {
-                when {
-                    anyOf{
-                        branch 'main'
-                        tag "*"
-                    }
-                }
-                steps {
-                    echo 'unit tests'
-                }
-
-            }
-
-
-            stage('Build packages') {
-                when { tag "*" }
-                steps {
-                    echo 'Build packages'
-                }
-
-            }
-
-
-            stage('Prepare Artifacts') {
-                when { tag "*" }
-                steps {
-                    echo 'prepare artifacts'
-                }
-
-            }
-
-
-            stage('Publish Artifacts') {
-                when { tag "*" }
-                steps {
-                    echo 'publish artifacts'
-                }
-
-            }
-
-        }
     }
 }
