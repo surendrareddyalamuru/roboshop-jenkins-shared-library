075f6b02cb0c01527fa59b6c053317f18406ddfc
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-03 11:48:22 +0530

    jenkinsfile

--- vars/common.groovy
+++ vars/common.groovy
@@ -29,40 +29,41 @@ def codechecks() {
 }
 
 def artifacts() {
-    if ( env.TAG_NAME ==~ ".*" ) {
+    if (env.TAG_NAME ==~ ".*") {
 
-    stage('Prepare Artifacts') {
-        if (env.APPTYPE == "nodejs") {
-            sh '''
+        stage('Prepare Artifacts') {
+            if (env.APPTYPE == "nodejs") {
+                sh '''
           npm install 
           zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js 
         '''
-        }
-        if (env.APPTYPE == "java") {
-            sh '''
+            }
+            if (env.APPTYPE == "java") {
+                sh '''
           mvn clean package 
           mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar 
           zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar
         '''
-        }
-        if (env.APPTYPE == "python") {
-            sh '''
+            }
+            if (env.APPTYPE == "python") {
+                sh '''
           zip -r ${COMPONENT}-${TAG_NAME}.zip *.py ${COMPONENT}.ini requirements.txt
         '''
-        }
-        if (env.APPTYPE == "nginx") {
-            sh '''
+            }
+            if (env.APPTYPE == "nginx") {
+                sh '''
               cd static
               zip -r ../${COMPONENT}-${TAG_NAME}.zip *
         '''
+            }
         }
-    }
 
         stage('publish Artifacts') {
             withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'nexusPass', usernameVariable: 'nexusUser')]) {
-        sh '''
+                sh '''
            curl -v -u ${nexusUser}:${nexusPass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
         '''
+            }
         }
     }
 }
