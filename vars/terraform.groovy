37e5fc6edf6739d84ad1314d751d7e429f4d6779
surendra reddy <surendrareddylamuru01@gmail.com>
2023-04-26 10:40:29 +0530

    jobs create

---
+++ vars/terraform.groovy
@@ -0,0 +1,33 @@
+def call() {
+    pipeline {
+        agent any
+
+        options {
+            ansiColor('xterm')
+        }
+
+        parameters {
+            choice(name: 'ENV', choices: ['', 'dev', 'prod'], description: 'Environment')
+            choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Action')
+        }
+
+        stages {
+
+            stage('Terraform') {
+                steps {
+                    sh '''
+            rm -rf .terraform*
+            terrafile -f env-${ENV}/Terrafile
+            terraform init -backend-config=env-${ENV}/backend.tfvars
+            terraform ${ACTION} -auto-approve -var-file=env-${ENV}/main.tfvars || true 
+            terraform ${ACTION} -auto-approve -var-file=env-${ENV}/main.tfvars || true 
+            terraform ${ACTION} -auto-approve -var-file=env-${ENV}/main.tfvars 
+        '''
+                }
+            }
+
+
+        }
+
+    }
+}
